30d181dde551bbb97242387ab0cee6542919146b
Ben Burkert <ben@benburkert.com>
2009-05-17 15:32:15 -0500

    Initial commit to hosebird.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'context'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'hosebird'
+
+class Test::Unit::TestCase
+end
