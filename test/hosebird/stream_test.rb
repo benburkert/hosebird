8dab91972296e44570718b68bcbbdb9633a86a45
Ben Burkert <ben@benburkert.com>
2009-05-18 11:41:41 -0500

    TDD time.

--- test/hosebird/stream_test.rb
+++ test/hosebird/stream_test.rb
@@ -4,13 +4,20 @@ module Hosebird
   class StreamTest < Test::Unit::TestCase
 
     before do
-      @stream = Hosebird::Stream('/example.json', :get).allocate
+      @stream = Hosebird::Stream('/example.json', :GET).allocate
       @stream.twitter = twitter_connection
+    end
+
+    context "#basic_auth" do
+      test "should start with 'Authorization: Basic'" do
+        assert_match(/\AAuthorization:\sBasic/, @stream.basic_auth)
+      end
     end
 
     context "#request" do
       test "should conform with RFC2616" do
         assert_match(/\AGET\s\/example\.json\sHTTP\/1\.1\r\n/m, @stream.request)
+        assert_match(/[\r][\n][\r][\n]\Z/, @stream.request)
       end
 
       test "should include the basic auth header" do
